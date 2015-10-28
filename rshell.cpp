d55c69795fe4c518b5926b691b2b72caddafde62
davidzhu94 <davidzhu94@gmail.com>
2015-10-28 12:34:28 -0700

    edited parseinator and added compilifier

--- rshell.cpp
+++ rshell.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <boost/tokenizer.hpp>
 #include <vector>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 using namespace std;
 using namespace boost;
-/*
-int main(){
-   string s;
-   getline(cin, s);
-   tokenizer<escaped_list_separator<char> >  tok(s);
-   for(tokenizer<escaped_list_separator<char> > ::iterator beg=tok.begin(); beg!=tok.end();++beg){
-       cout << *beg << "\n";
-   }
-}
-*/
+
+void parseinator(vector<string> input, string& command, string& argument, int& position);
+void commandifier(string command, string argument);
+
 int main()
 {
     string str;
+    string command;
+    string argument;
+    int position = 0;
     getline(cin, str);
-    vector<string> command;
+    vector<string> instruction;
     typedef tokenizer<boost::char_separator<char> > Tok;
     char_separator<char> sep; // default constructed
     Tok tok(str, sep);
     for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
-        command.push_back(*tok_iter);
-    for(int i = 0; i < command.size(); i++)
-        cout << command[i] << endl;
-    return 0;
+        instruction.push_back(*tok_iter);
+    for(int i = 0; i < instruction.size(); i++)
+        cout << instruction[i] << endl;
+    for(; position < instruction.size();)
+    {
+        parseinator(instruction, command, argument, position);
+        commandifier(command, argument);
+        command = "";
+        argument = "";
+    }
 }
 
-
-string parseinator(vector<string> input,  int& position)
+void parseinator(vector<string> input, string& command, string& argument, int& position)
 {
-    string output;
+    command = input[position];
+    position++;
     for(; position < input.size(); position++)
     {
         if(input[position] == "-;")
@@ -45,8 +53,55 @@ string parseinator(vector<string> input,  int& position)
             position += 2;
             break;
         }
-        output += input[position];
-        output += " ";
+        if(input[position] == "#")
+        {
+            position = input.size();
+            break;
+        }
+        argument += input[position];
+        argument += " ";
     }
-    return output;
-} 
+}
+
+void commandifier(string command, string argument)
+{
+    const char * const_command = command.c_str();
+    char char_command[command.size()];
+    const char * const_argument = argument.c_str();
+    char char_argument[argument.size()];
+    strcpy (char_command, const_command);
+    strcpy (char_argument, const_argument);
+    char * args[3] = { char_command, char_argument, NULL} ;
+    pid_t c_pid, pid;
+    int status;
+
+    c_pid = fork();
+
+    if( c_pid < 0)
+    {
+        perror("fork failed");
+        exit(1);
+    }
+
+    else if (c_pid == 0)
+    {
+
+        printf("Child: executing ls\n");                                                                                             
+        execvp( args[0], args);                                                                                                      
+        perror("execve failed");
+
+    }
+    else if (c_pid > 0)
+    {
+        if( (pid = wait(&status)) < 0)
+        {
+            perror("wait");
+            exit(1);
+        }
+
+        printf("Parent: finished\n");
+        
+    }
+   // return 0;
+}
+ 
