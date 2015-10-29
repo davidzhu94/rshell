75b79b7b750b524d809c4c466b4444fe9203ea9d
davidzhu94 <davidzhu94@gmail.com>
2015-10-29 01:29:54 -0700

    working program except minor issues with connectors

--- rshell.cpp
+++ rshell.cpp
@@ -10,104 +10,125 @@
 using namespace std;
 using namespace boost;
 
-void parseinator(vector<string> input, string& command, string& argument, int& position);
-void commandifier(string command, string argument);
 
 class Connector
 {
-    public:
+public:
     Connector();
     ~Connector();
     bool run;
-    virtual void runNext(bool succeed)= 0;
+    int type;
+    bool runNext();
 };
 
 Connector::Connector()
 {
-    run = false;
+    run = true;
+    type = 0;
 }
 
 Connector::~Connector()
+{   
+}
+bool Connector::runNext()
 {
+    if (type == 1)
+    {
+        if(!run)
+        {
+            return false;
+        }
+    }
+    else if(type == 2)
+    {
+        if(run)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
-class And: public Connector
-{
-    void runNext(bool succeed);
-};
+void parseinator(vector<string> input, string& command, string& argument, int& position, Connector& connect);
+void commandifier(string  command, string  argument, Connector& connect);
 
-void And::runNext(bool succeed)
-{
-    if(succeed)
-        run = true;
-    else
-        run = false;
-}
-
-class Or: public Connector
-{
-    void runNext(bool succeed);
-};
-
-void Or::runNext(bool succeed)
-{
-    if(succeed)
-        run = false;
-    else
-        run = true;
-}
-
-class Do: public Connector
-{
-    void runNext(bool succeed);
-};
-
-void Do::runNext(bool succeed)
-{
-    run = true;
-}
 int main()
 {
+    
     string str;
     string command;
     string argument;
+    Connector connect;
     char hostname[100];
     int position = 0;
-    cout<<getlogin();
-    gethostname(hostname,sizeof hostname);
-    cout<<"@"<<hostname<<"$ ";
-    getline(cin, str);
-    vector<string> instruction;
-    typedef tokenizer<boost::char_separator<char> > Tok;
-    char_separator<char> sep; // default constructed
-    Tok tok(str, sep);
-    for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
-        instruction.push_back(*tok_iter);
-    for(int i = 0; i < instruction.size(); i++)
-        cout << instruction[i] << endl;
-    for(; position < instruction.size();)
+    while(true)
     {
-        parseinator(instruction, command, argument, position);
-        commandifier(command, argument);
+        cout<<getlogin();
+        gethostname(hostname,sizeof hostname);
+        cout<<"@"<<hostname<<"$ ";
+        getline(cin, str);
+        if (str == "exit")
+            break;
+        vector<string> instruction;
+        typedef tokenizer<boost::char_separator<char> > Tok;
+        char_separator<char> sep; // default constructed
+        Tok tok(str, sep);
+        for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
+            instruction.push_back(*tok_iter);
+       // for(int i = 0; i < instruction.size(); i++)
+        //    cout << instruction[i] << endl;
+        parseinator(instruction, command, argument, position, connect);
+        commandifier(command, argument, connect);
         command = "";
         argument = "";
+        for(; position < instruction.size();)
+        {
+            parseinator(instruction, command, argument, position, connect);
+            if(connect.runNext())
+                commandifier(command, argument, connect);
+            command = "";
+            argument = "";
+        }
+        position = 0;
     }
+    return 0;
 }
 
-void parseinator(vector<string> input, string& command, string& argument, int& position)
+void parseinator(vector<string> input, string& command, string& argument, int& position, Connector& connect)
 {
     command = input[position];
     position++;
     for(; position < input.size(); position++)
     {
+
+        if(input[position] == "-")
+        {
+            while(input[position] != ";" && input[position] != "|" && input[position] != "&" && position+1 < input.size())
+            {
+                argument += input[position];
+                position++;
+            }
+            if(position+1 == input.size())
+            {
+                argument += input[position];
+                position++;
+                break;
+            }
+        }
         if(input[position] == ";")
         {
-	
             position++;
             break;
         }
-        if(input[position] == "|" || input[position] == "&")
+        if(input[position] == "&")
         {
+            connect.type = 1;
+            position += 2;
+            break;
+        }
+        if(input[position] == "|")
+        {
+            connect.type = 2;
             position += 2;
             break;
         }
@@ -121,15 +142,27 @@ void parseinator(vector<string> input, string& command, string& argument, int& p
     }
 }
 
-void commandifier(string command, string argument)
+void commandifier(string command, string argument, Connector& connect)
 {
+    cout << "i be command >>" << command << "<< i be argument >>" << argument << "<<" << endl;
     const char * const_command = command.c_str();
     char char_command[command.size()];
     const char * const_argument = argument.c_str();
     char char_argument[argument.size()];
     strcpy (char_command, const_command);
-    strcpy (char_argument, const_argument);
-    char * args[3] = { char_command, char_argument, NULL} ;
+    strcpy (char_argument, const_argument);    
+    char * args[2];
+    char * args1[3];
+    bool no_arg = true;
+    bool failed = false;
+    cout<<argument.size();
+    if(argument.size() ==0)
+	args = { char_command, NULL};
+    else
+    {
+	no_arg = false;
+	args1 = { char_command, char_argument, NULL} ;
+    }
     pid_t c_pid, pid;
     int status;
 
@@ -144,14 +177,27 @@ void commandifier(string command, string argument)
     else if (c_pid == 0)
     {
 
-        printf("Child: executing ls\n");                                                                                             
-        execvp( args[0], args);
-        if(execvp( args[0], args) == -1)
-        {
-            
-        }                                                                                                      
-        perror("execve failed");
-
+        printf("Child: executing ls\n");
+ 	if(no_arg)
+	{                                                                                             
+        	execvp( args[0], args);
+        	if(execvp( args[0], args) == -1) 
+                {                                                                                                     
+                    perror("execve failed");
+                    connect.run = false;
+                    failed = true;
+                }
+	}
+	else
+	{
+ 		execv(args1[0],args1);
+		if(execvp(args1[0],args1) == -1)
+                {
+                    perror("exeve failed");
+                    connect.run = false;
+                    failed = true;
+                }
+	}
     }
     else if (c_pid > 0)
     {
@@ -162,7 +208,10 @@ void commandifier(string command, string argument)
         }
 
         printf("Parent: finished\n");
+
         
     }
+    if(!failed)
+        connect.run = true;
    // return 0;
 }
