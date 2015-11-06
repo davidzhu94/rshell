409678cc18bf08fba6d210c0d41d79acd75b7474
schecter22107 <jorgeflorescollege@gmail.com>
2015-11-06 15:44:51 -0800

    Added comments and Fixed style

--- rshell.cpp
+++ rshell.cpp
@@ -10,21 +10,21 @@
 using namespace std;
 using namespace boost;
 
-
 class Connector
 {
-public:
-    Connector();
-    ~Connector();
-    bool run;
-    int type;
-    bool runNext();
+    public:
+        Connector();
+        ~Connector();
+        bool run;
+        int type;
+        bool runNext();
 };
 
 Connector::Connector()
-{
-    run = true;
-    type = 0;
+{ 
+    // the semicolon connector allows the next command to be execute always
+    run = true; 
+    type = 0; // semicolon connector set to 0 
 }
 
 Connector::~Connector()
@@ -32,26 +32,24 @@ Connector::~Connector()
 }
 bool Connector::runNext()
 {
-    if (type == 1)
+    if (type == 1) // && connector 
     {
-        if(!run)
+        if (!run) // if the first command doesn't succeed  it wont run the next command
         {
             return false;
         }
     }
-    else if(type == 2)
+    else if (type == 2) // || connector
     {
-        if(run)
+        if (run) // if the first command succeed it wont run the next command
         {
             return false;
         }
     }
     return true;
 }
-
 void parseinator(vector<string> input, string& command, string& argument, int& position, Connector& connect);
-void commandifier(string  command, string  argument, Connector& connect);
-
+void commandifier(string command, string argument, Connector& connect);
 int main()
 {
     
@@ -61,34 +59,40 @@ int main()
     Connector connect;
     char hostname[100];
     int position = 0;
-    while(true)
+    while (true)
     {
-        cout<<getlogin(); // returns string containing name of user logged in
-        gethostname(hostname,sizeof hostname);
-        cout<<"@"<<hostname<<"$ ";
-        getline(cin, str);
-        if (str == "exit")
+        cout << getlogin(); // returns string containing name of user logged in
+        gethostname(hostname, sizeof hostname); // returns the standard host name for the current machine
+        cout << "@" << hostname << "$ ";
+        getline(cin, str); 
+        if (str == "exit") // special command of exit 
+        {
             break;
-        vector<string> instruction;
+        }
+        vector<string> instruction; // vector of strings 
         typedef tokenizer<boost::char_separator<char> > Tok;
         char_separator<char> sep; // default constructed
         Tok tok(str, sep);
-            for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
-            instruction.push_back(*tok_iter);
+        for (Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
+        {
+	    instruction.push_back(*tok_iter);
+	}
        // for(int i = 0; i < instruction.size(); i++)
         //    cout << instruction[i] << endl;
-        if(instruction[0] != "#")
+        if (instruction[0] != "#") // checks for user input except for comments (#)
         {    
-            parseinator(instruction, command, argument, position, connect);
+            parseinator(instruction, command, argument, position, connect); 
             commandifier(command, argument, connect);
             command = "";
             argument = "";
-            for(; position < instruction.size();)
+            for (; position < instruction.size();)
             {
                 parseinator(instruction, command, argument, position, connect);
-                if(connect.runNext())
+                if (connect.runNext())
+		{
                     commandifier(command, argument, connect);
-                command = "";
+                }
+		command = "";
                 argument = "";
             }
             position = 0;
@@ -104,46 +108,48 @@ void parseinator(vector<string> input, string& command, string& argument, int& p
    //     connect.type = 1;
    //     connect.run = false;
    // }
-    if(input[position] == "&")
+    if (input[position] == "&") // check if string is equal to & connector 
     {
-        connect.type = 1;
-        position += 2;
+        connect.type = 1; // set & connector to 1 
+        position += 2; // +=2 in order to take && connect
     }
-    if(input[position] == "|")
+    if (input[position] == "|")
     {
-        connect.type = 2;
-        position += 2;
+        connect.type = 2; // set | connector to 2
+        position += 2; // +=2 in order to take || connector
     }
-    if(input[position] == ";")
+    if (input[position] == ";")
     {
-        connect.type = 0;
+        connect.type = 0; // set ; connector to 0 
         position ++;
     }
-    if(input[position] != "#")
+    if (input[position] != "#")
     {
         command = input[position];
         position++;
     }
-    for(; position < input.size(); position++)
+    for (; position < input.size(); position++)
     {
-        if(input[position] == "#")
+        if (input[position] == "#")
         {
-              cout << "I AM IN HERE" << endl;
+            cout << "I AM IN HERE" << endl;
             position = input.size();
             connect.type = 1;
             connect.run = false;
             break;
         }
-        if(input[position] == "&" || input[position] == "|" || input[position] == ";")
+        if (input[position] == "&" || input[position] == "|" || input[position] == ";")
+	{
             break;
-        if(input[position] == "-")
+	}
+        if (input[position] == "-")
         {
-            while(input[position] != ";" && input[position] != "|" && input[position] != "&" && position+1 < input.size())
+            while (input[position] != ";" && input[position] != "|" && input[position] != "&" && position+1 < input.size())
             {
                 argument += input[position];
                 position++;
             }
-            if(position+1 == input.size() || input[position] == ";" || input[position] == "|" || input[position] == "&");
+            if (position+1 == input.size() || input[position] == ";" || input[position] == "|" || input[position] == "&");
             {
                // argument += input[position];
                 position++;
@@ -152,81 +158,81 @@ void parseinator(vector<string> input, string& command, string& argument, int& p
             break;
         }
         argument += input[position];
-        if(position+1 != input.size() && command == "echo")
+        if (position+1 != input.size() && command == "echo")
+  	{
             argument += " ";
+	}
     }
 }
-
 void commandifier(string command, string argument, Connector& connect)
 {
     cout << "i be command >>" << command << "<< i be argument >>" << argument << "<<" << endl;
-    const char * const_command = command.c_str();
+    const char * const_command = command.c_str(); 
     char char_command[command.size()];
     const char * const_argument = argument.c_str();
     char char_argument[argument.size()];
     strcpy (char_command, const_command);
     strcpy (char_argument, const_argument);    
-    char * args[2];
-    char * args1[3];
+    char * args[2]; // char pointer array that holds command, and NULL
+    char * args1[3]; // char pointer array that holds command, argument, and NULL
     bool no_arg = true;
     bool failed = false;
-    cout<<argument.size();
-    if(argument.size() ==0)
-	args = { char_command, NULL};
+    cout << argument.size();
+
+    if (argument.size() == 0) // no arguments 
+    {
+        args = {char_command, NULL}; // args will contain the command
+    }
     else
     {
-	no_arg = false;
-	args1 = { char_command, char_argument, NULL} ;
+	no_arg = false; // sets bool no_arg to false 
+	args1 = { char_command, char_argument, NULL} ; // args1 contains command, and argument
     }
-    pid_t c_pid, pid;
+    pid_t c_pid, pid; // data type to reporesent process ID's
     int status;
+    c_pid = fork(); // create our fork  
 
-    c_pid = fork();
-
-    if( c_pid < 0)
+    if (c_pid < 0) // indicates the fork has fail if its less than 0 
     {
         perror("fork failed");
         exit(1);
     }
-
-    else if (c_pid == 0)
+    else if (c_pid == 0) // in child process 
     {
 
-        printf("Child: executing ls\n");
- 	if(no_arg)
+        cout << "Child: executing ls" << endl;
+ 	if (no_arg) // no argument
 	{                                                                                             
-        	execvp( args[0], args);
-        	if(execvp( args[0], args) == -1) 
+        	execvp( args[0], args); // execvp the char pointer array that holds the command only
+        	if (execvp(args[0], args) == -1) // if it returns -1 it failed
                 {                                                                                                     
-                    perror("execve failed");
+                    perror("execvp failed");
                     connect.run = false;
                     failed = true;
                 }
 	}
-	else
+	else // with command and argument
 	{
- 		execv(args1[0],args1);
-		if(execvp(args1[0],args1) == -1)
+ 		execvp(args1[0], args1); // execvp the char pointer array that holds the command, and argument
+		if (execvp(args1[0], args1) == -1) // if it returns -1 it failed
                 {
-                    perror("exeve failed");
-                    connect.run = false;
+                    perror("execvp failed");
+                    connect.run = false; 
                     failed = true;
                 }
 	}
     }
-    else if (c_pid > 0)
+    else if (c_pid > 0) // parent process
     {
-        if( (pid = wait(&status)) < 0)
+        if ((pid = wait(&status)) < 0) 
         {
             perror("wait");
             exit(1);
         }
-
-        printf("Parent: finished\n");
-
-        
+        cout << "Parent: finished" << endl;   
     }
-    if(!failed)
+    if (!failed)
+    {
         connect.run = true;
-   // return 0;
+    }
 }
